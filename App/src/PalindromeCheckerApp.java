c6c409e64d4313ef3e2ef2671826c62d3424050f
mithun-bit <my6506@srmist.edu.in>
2026-03-16 14:30:31 +0530

    palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,15 +1,19 @@
 public class PalindromeCheckerApp{
-    /**
-     * Application entry point,
-     *
-     * this is a java Palindrome Checker App welcome message
-     *
-     * @author Developer
-     * @Version 1.0
-     */
+
     public static void main(String[] args){
-        System.out.println("welcome to the palindrome checker management system");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "madam";   // Hardcoded string
+        boolean isPalindrome = true;
+
+// Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
     }
 }
