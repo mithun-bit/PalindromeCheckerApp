57e8f1a894a21d86d8c41f4488518cc49a541b66
mithun-bit <my6506@srmist.edu.in>
2026-03-16 16:33:04 +0530

    uc11 oop palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,23 @@
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input) {
+
+        // Initialize pointers
+        int start = 0;
+        int end = input.length() - 1;
+
+        // Compare characters moving inward
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
 public class PalindromeCheckerApp{
 
     public static void main(String[] args){
